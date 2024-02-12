8fc047dfe2b6537f4b39eaea65b879865cd5109e
disisdsid <siddhanta.dash@gmail.com>
2024-02-12 16:03:22 +0530

    edited config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Siddhanta Dash is interviewing for Systemone LLC right now. Omg, Server got hacked!!');
 });
 
 app.listen(PORT, HOST, () => {
